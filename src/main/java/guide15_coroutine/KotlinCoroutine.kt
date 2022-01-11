079ef81afe8ef61400941a26d091ea55b57e227b
qonyqian <qonyqian@tencent.com>
2022-01-11 14:51:17 +0800

    更新协程

--- src/main/java/guide15_coroutine/KotlinCoroutine.kt
+++ src/main/java/guide15_coroutine/KotlinCoroutine.kt
@@ -16,7 +16,7 @@ import kotlin.coroutines.suspendCoroutine
  *
  * 一、协程五要素
  * 1.协程作用域
- * 2.协程作用域的扩展函数
+ * 2.协程作用域的扩展函数（launch、async）
  * 3.协程上下文
  * 4.协程启动模式
  * 5.lambda表达式
@@ -36,51 +36,58 @@ import kotlin.coroutines.suspendCoroutine
  * 2.父协程需等待子协程执行完毕后才会最终进入完成状态，而不管父协程本身的代码块是否已执行完
  * 3.子协程会继承父协程上下文中的元素，如果自身有相同 Key 的成员，则覆盖对应 Key，覆盖效果仅在自身范围内有效
  *
- * 三、使用 Delay 函数延迟协程执行
- * 1.delay 函数是一个非阻塞式挂起函数，它可以让当前协程延迟到指定的时间执行，且只能在协程的作用域或者其他挂起函数中调用
+ * 二、使用 CoroutineScope 函数创建一个协程作用域
+ * 使用颇多，单纯就是为了创建一个协程作用域。
  *
- * 四、使用 runBlocking 函数创建一个能阻塞当前线程的协程作用域
- * 1.runBlocking 函数可以保证在协程作用域内的所有代码和子协程没有全部执行完之前一直阻塞当前线程
+ * 三、使用 coroutineScope 函数执行一段协程
+ * coroutineScope 函数为了方便地执行一个协程，自动在内部创建了一个协程作用域，使调用者只需要专注协程的业务逻辑。
+ * 1.coroutineScope 函数是一个挂起函数，它的参数也是一个挂起函数。
+ * 2.coroutineScope 函数会继承外部的协程作用域并创建一个子作用域
+ * 3.coroutineScope 函数只是为了方便地执行一个协程，它的返回结果是一个具体数据。
  *
- * 五、使用 launch 函数在当前的协程作用域下创建子协程
- *
- * 六、使用 suspend 关键字将一个函数声明成挂起函数
- * 1.suspend 关键字能将一个函数声明成挂起函数
- * 2.挂起函数必须在协程或者另一个挂起函数里被调用
- *
- * 七、使用 coroutineScope 函数创建一个协程作用域
- * 1.coroutineScope 函数会继承外部的协程作用域并创建一个子作用域
- * 2.coroutineScope 函数也是一个挂起函数，因此我们可以在任何其他挂起函数中调用
- *
- * 八、使用 async 函数创建一个子协程并获取执行结果
+ * 四、使用 launch 函数在当前的协程作用域下创建子协程
  * launch 函数可以创建一个子协程，但是 launch 函数只能用于执行一段逻辑，却不能获取执行的结果，因为它的返回值永远是一
- * 个 Job 对象，那么如果我们想创建一个子协程并获取它的执行结果，我们可以使用 async 函数。
- * 1.async 函数必须在协程作用域下才能调用
+ * 个 Job 对象。
+ *
+ * 五、使用 async 函数在当前的协程作用域下创建一个子协程，并获取协程执行结果
+ * 我们可以使用 async 函数，创建一个子协程并获取它的执行结果。
+ * 1.async 函数 和 launch 函数都是协程作用域的扩展函数
  * 2.async 函数会创建一个子协程并返回一个 Deferred 对象，如果需要获取 async 函数代码块中的执行结果，只需要调用 Deferred 对
  * 象的 await() 方法即可
  * 3.async 函数在调用后会立刻执行，当调用 await() 方法时，如果代码块中的代码还没执行完，那么 await() 方法会将当前协程阻塞住，直
  * 到可以获取 async 函数中的执行结果
  *
- * 九、使用 withContext 函数构建一个简化版的 async 函数
+ * 六、Kotlin中的挂起
+ * Kotlin中的挂起，挂起的是协程，底层的线程不受影响，将继续执行协程下面的代码。而当被挂起的协程恢复运行后，协程框架会自动切换回原来的
+ * 线程，并且从协程里被挂起的地方继续向下执行。
+ *
+ * 七、使用 suspend 关键字将一个函数声明成挂起函数
+ * 1.suspend 关键字能将一个函数声明成挂起函数
+ * 2.挂起函数必须在协程或者另一个挂起函数里被调用
+ *
+ * 八、使用 Delay 函数延迟协程执行
+ * 1.delay 函数是一个非阻塞式挂起函数，它可以让当前协程延迟到指定的时间执行，且只能在协程的作用域或者其他挂起函数中调用
+ *
+ * 九、使用 runBlocking 函数创建一个能阻塞当前线程的协程作用域
+ * 1.runBlocking 函数可以保证在协程作用域内的所有代码和子协程没有全部执行完之前一直阻塞当前线程
+ *
+ * 十、使用 withContext 函数构建一个简化版的 async 函数
  * 1.withContext 函数是一个挂起函数，并且强制要求我们指定一个协程上下文参数，这个调度器其实就是指定协程具体的运行线程
  * 2.withContext 函数在调用后会立刻执行，它可以保证其作用域内的所有代码和子协程在全部执行完之前，一直阻塞当前协程
  * 3.withContext 函数会创建一个子协程并将最后一行的执行结果作为返回值
  *
- * 十、使用 suspendCoroutine 函数简化回调的写法
+ * 十一、使用 suspendCoroutine 函数简化回调的写法
  * 1.suspendCoroutine 函数必须在协程作用域或者挂起函数中调用，它接收一个 Lambda 表达式，主要作用是将当前协程立即挂起，然后
  * 在一个普通线程中去执行 Lambda 表达式中的代码
  * 2.suspendCoroutine 函数的 Lambda 表达式参数列表会传入一个 Contination 参数，调用它的 resume() 或 resumeWithException()
  * 方法可以让协程恢复执行
  *
- * 十一、Kotlin中的挂起
+ * 十二、Kotlin中的调度器
  * Dispatchers 调度器，它可以将协程限制在一个特定的线程执行，或者将它分派到一个线程池，或者让它不受限制地运行。常用的
  * Dispatchers ，有以下三种：
  * 1.Dispatchers.Main：Android 中的主线程
  * 2.Dispatchers.IO：针对磁盘和网络 IO 进行了优化，适合 IO 密集型的任务，比如：读写文件，操作数据库以及网络请求
  * 3.Dispatchers.Default：适合 CPU 密集型的任务，比如计算
- *
- * Kotlin中的挂起，挂起的是协程，底层的线程不受影响，将继续执行协程下面的代码。而当被挂起的协程恢复运行后，协程框架会自动切换回原来的
- * 线程，并且从被挂起的地方继续向下执行。
  *
  * 总结：
  * 1、被 suspend 修饰的挂起函数比普通函数多两个操作：
@@ -242,7 +249,7 @@ suspend fun request(url: String): String {
 //具体使用
 suspend fun getBaiduResponse() {
     try {
-        val request = request("https://www.baidu.com/")
+        val response = request("https://www.baidu.com/")
     } catch (e: Exception) {
         //对异常情况进行处理
     }
