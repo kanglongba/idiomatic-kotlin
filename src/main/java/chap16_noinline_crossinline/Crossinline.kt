c3762c9eb6f08e69360068faa5c4dc07a320130c
qianhailong <qianhailong@corp.netease.com>
2022-08-18 11:04:11 +0800

    修改noinline、crossinline和伴生类的注释

--- src/main/java/chap16_noinline_crossinline/Crossinline.kt
+++ src/main/java/chap16_noinline_crossinline/Crossinline.kt
@@ -1,12 +1,25 @@
 package chap16_noinline_crossinline
 
+//一个函数中，如果存在一个lambda表达式，在该lambda中不支持直接进行return退出.支持通过标签，退出lambda表达式(局部退出)。
+//如果是具名函数或匿名函数，则支持直接进行return，退出当前函数（但不会退出外层函数）。
 fun someHigherOrderFunction(message: String, func: (String) -> Unit) {
+}
+
+//但是如果函数是内联的，那么它的lambda表达式支持非局部控制流，即可直接使用裸return
+inline fun someInlineHigherOrderFunction(message: String, func: (String) -> Unit) {
+    func(message)
 }
 
 //直接在lambda返回外部函数的情况称为非局部返回
 // https://www.kotlincn.net/docs/reference/inline-functions.html
-// 在 Kotlin 中，我们只能对具名或匿名函数使用正常的、非限定的 return 来退出。 这意味着要退出一个 lambda 表达式，我们必须使用一个标签，并且在 lambda 表达式内部禁止使用裸 return
+// 在 Kotlin 中，我们只能对具名或匿名函数使用正常的、非限定的 return 来退出。 这意味着要退出一个 lambda 表达式，我们必须使用一个标签，并且在 lambda 表达
+// 式内部禁止使用裸 return
 // crossinline 标识该 lambda 表达式中不允许非局部控制流
+//
+// 首先，非内联函数的lambda表达示，不支持非局部返回（不能使用裸return，只能使用标签）
+// 其次，内联函数的lambda表达式，没有这个限制，支持非局部返回（可以使用裸return，但由于内联，会直接返回外层函数）
+// 所以，crossinline关键字，就用来标识在内联函数中，禁止lambda表达式使用非局部返回（本意是为了防止意外return，所以就干脆禁止了这种行为，那么就彻底没有这个风险了）
+// crossinline 必须 与 inline（内联函数）配合使用
 inline fun crossInlineTest(message: String, crossinline func: (String) -> Unit) {
     someHigherOrderFunction("crossInlineTest") {
         func(it) // Compile error here
@@ -18,6 +31,14 @@ fun main(arg: Array<String>) {
         println(it)
         // Uncomment to experience compile error
         //return // Compile error here because crossinline does not allow return
+    }
+    var func = fun() { //匿名函数
+        return
+    }
+    func()
+    someInlineHigherOrderFunction("hello") {
+        println(it)
+        return
     }
     println("Main finished")
 }
--- src/main/java/chap16_noinline_crossinline/Noinline.kt
+++ src/main/java/chap16_noinline_crossinline/Noinline.kt
@@ -1,8 +1,8 @@
 package chap16_noinline_crossinline
 
 // https://www.jianshu.com/p/cd0be9b887ec
-// https://blog.csdn.net/u013009899/article/details/78584994
 // 关闭传给内联函数的 lambda 表达式的内联。即切断内联函数的传导。
+// 说白了，就是让内联函数的lambda参数不进行内联，保留一般函数的特征。
 // noinline 和 crossinline 都是用来修饰lambda表达式的
 inline fun <reified T> doSomething(obj: T, noinline func: (T) -> T) {
     func(obj)
